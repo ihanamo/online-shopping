2f6cdaeb7c6d66fc94cf3d69caccdd347458c710
ihanamo <hnnemntzr200@gmail.com>
2024-07-23 15:08:43 +0330

    authenticate user and login added

--- handlers/customer.go
+++ handlers/customer.go
@@ -31,6 +31,26 @@ func GenerateJWT(customer models.Customer) (string, error) {
 	return tokenStr, err
 }
 
+func AuthenticateCustomer(username, password string) (models.Customer, string, error) {
+	var customer models.Customer
+	result := database.DB.Where("username = ?", username).First(&customer)
+	if result.Error != nil {
+		return customer, "", result.Error
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(customer.Password), []byte(password))
+	if err != nil {
+		return customer, "", echo.NewHTTPError(http.StatusUnauthorized, "Invalid password")
+	}
+
+	token, err := GenerateJWT(customer)
+	if err != nil {
+		return customer, "", echo.NewHTTPError(http.StatusInternalServerError, "Failed to Generate Token")
+	}
+
+	return customer, token, nil
+}
+
 func CreateCustomer(c echo.Context) error {
 	log.Println("Create Customer called")
 	customer := new(models.Customer)
@@ -51,6 +71,24 @@ func CreateCustomer(c echo.Context) error {
 	}
 	log.Println("user created")
 	return c.JSON(http.StatusCreated, customer)
+}
+
+func Login(c echo.Context) error {
+	credentials := new(models.Credentials)
+	if err := c.Bind(credentials); err != nil {
+		return err
+	}
+
+	customer, token, err := AuthenticateCustomer(credentials.Username, credentials.Password)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, echo.Map{"message": "Invalid username or password"})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "Login successful",
+		"token":   token,
+		"user":    customer,
+	})
 }
 
 func ReadCustomer(c echo.Context) error {
